8146160999556b2d1329d46e2a674ebd300e40eb
Anil Sarma <anilsarma@yahoo.com>
2014-07-31 23:17:04 -0400

    a running copy of the proxy server, user must generate the certificates

--- ssl/samples/ssl.proxy.py
+++ ssl/samples/ssl.proxy.py
@@ -3,61 +3,110 @@
 
 
 # work in progress does not yet work.
+# usefull to encrypt a connection over a public connection
+# both ends of the connections is authenticated 
+
 import socket, ssl
 import pprint
-import sys
+import sys, time
 import threading
+import getopt
+from datetime import datetime
 
 
-PORT = 10024
+LISTEN_PORT = 10024
+REMOTE_PORT = 10025
+REMOTE_HOST="localhost"
+REMOTE_CERT="client"
+MY_CERT="server"
+SERVER_MODE=True
+VERBOSE=False
+VERSION="0.1"
 
-print "starting SSL(incoming) proxy server on %d" % (PORT)
+def usage():
+	print sys.argv[0], " <options>"
+	print "\t VERSION ", VERSION
+	print "\t-client|-server -cert <my certificate prefix> -remote_cert <remote cert prefix>"
+	print "\t--listen <port> --remote_host <hostname> --remote_port <port>" 
+	print "\t--verbose"
+	sys.exit(1)
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "", ["help", "verbose", "client", 
+			"server", "cert=", "remote_cert=", "listen=", "port=", "remote_host=", "host=", "remote_port="])
+except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+   	usage()
+        sys.exit(2)
+
+output = None
+verbose = False
+for o, a in opts:
+        if o == "--help":
+            usage()
+            sys.exit()
+        if o == "--help":
+            VERBOSE=True
+        elif o == "--server":
+	    SERVER_MODE = True 
+        elif o == "--client":
+	    SERVER_MODE = False 
+        elif o == "--cert":
+	    MY_CERT = a 
+        elif o == "--remote_cert":
+	    REMOTE_CERT = a 
+        elif o in ("--listen", "--port"):
+	    LISTEN_PORT = int(a) 
+        elif o == "--remote_port":
+	    REMOTE_PORT = int(a) 
+        elif o in( "--remote_host", "--host"):
+	    REMOTE_HOST = a 
+        else:
+            assert False, "unhandled option"
+
+print "Running in Server Mode", (SERVER_MODE)
+
+
+
+print sys.argv[0], " VERSION ", VERSION
+print "starting SSL(incoming) proxy server SERVER PORT %d, Remote Port %d" % (LISTEN_PORT, REMOTE_PORT)
 bindsocket = socket.socket()
 bindsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-bindsocket.bind(('', PORT))
+bindsocket.bind(('', LISTEN_PORT))
 bindsocket.listen(5)
 
+def hexdump(src, length=16):
+	FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or '.' for x in range(256)])
+	lines = []
+	for c in xrange(0, len(src), length):
+		chars = src[c:c+length]
+		hex = ' '.join(["%02x" % ord(x) for x in chars])
+		printable = ''.join(["%s" % ((ord(x) <= 127 and FILTER[ord(x)]) or '.') for x in chars])
+		lines.append("%04x %-*s %s\n" % (c, length*3, hex, printable))
+		return ''.join(lines)
 
-
-def do_something(connstream, data):
-    print "do_something:", data
-    return False
-
-def deal_with_client(connstream):
-    data = connstream.read()
-    while data:
-        if not do_something(connstream, data):
-            break
-        data = connstream.read()
-
-
-def process_incoming_ssl(input, output):
+def process_sockets(input, output):
     data = input.recv(1024)
     try:
         while data:
-            output.send(data);
+	    if VERBOSE:
+	    	print datetime.now().strftime("%H:%M:%S.%f"), "\n", hexdump(src=data)
+            output.send(data)
             data = input.recv(1024)
     except:
         print "exception process_incoming_ssl:" , sys.exc_info()
         try:
+            output.shutdown(socket.SHUT_RDWR)
+            output.close()
+        except:
+            pass
+        try:
+            input.shutdown(socket.SHUT_RDWR)
             input.close()
         except:
             pass
         
-def process_outgoing_ssl(input, output):
-    data = input.recv(1024)
-    try:
-        while data:
-            print data
-            output.send(data);
-            data = input.recv(1024)
-    except:
-        try:
-            input.close()
-        except:
-            pass
-        print "exception process_outgoing_ssl:" , sys.exc_info()
-
     
 
 while True:
@@ -66,39 +115,67 @@ while True:
     
     try:
         # we will verify both sides of the socket.
-        connstream = ssl.wrap_socket(newsocket,
+	if SERVER_MODE:
+		print "securing server connection .. "
+        	connstream = ssl.wrap_socket(newsocket,
                                      server_side=True,
-                                     certfile="server.crt",
-                                     keyfile ="server.key",
+                                     certfile=MY_CERT + ".crt",
+                                     keyfile =MY_CERT + ".key",
                                      
-                                     ca_certs="client.crt",
+                                     ca_certs=REMOTE_CERT+".crt",
                                      cert_reqs=ssl.CERT_REQUIRED
                                      )
+	else:
+		connstream = newsocket
         
         # at this point the far end has been authenticated now we'll start a connection
         # to the next leg(un-encrypted)
         other_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        other_socket.connect(("localhost", 22))
+        other_socket.connect((REMOTE_HOST, REMOTE_PORT))
+	if not SERVER_MODE:
+		print "securing client connection .. "
+        	other_socket = ssl.wrap_socket(other_socket,
+                                     server_side=False,
+                                     certfile=MY_CERT + ".crt",
+                                     keyfile =MY_CERT + ".key",
+                                     
+                                     ca_certs=REMOTE_CERT+".crt",
+                                     cert_reqs=ssl.CERT_REQUIRED
+                                     )
+        
 
-        t1 = threading.Thread(target=process_incoming_ssl, args = (connstream, other_socket) );
+
+        t1 = threading.Thread(target=process_sockets, args = (connstream, other_socket) );
         t1.daemon = True
         t1.start()
 
-        t2 = threading.Thread(target=process_outgoing_ssl, args = (other_socket, connstream) );
+        t2 = threading.Thread(target=process_sockets, args = (other_socket, connstream) );
         t2.daemon = True
         t2.start()
     
     except:
         print "exception:" , sys.exc_info()
+	try:
+        	connstream.shutdown(socket.SHUT_RDWR)
+        	connstream.close()
+	except:
+		pass
+	try:
+        	other_socket.shutdown(socket.SHUT_RDWR)
+        	other_socket.close()
+	except:
+		pass
         continue
 
     try:
-        print repr(connstream.getpeername())
-        print connstream.cipher()
-        print "PEER CERT", pprint.pformat(connstream.getpeercert())
+        #print repr(connstream.getpeername())
+        #print connstream.cipher()
+        #print "PEER CERT", pprint.pformat(connstream.getpeercert())
+	pass
     
         #deal_with_client(connstream)
     finally:
-        connstream.shutdown(socket.SHUT_RDWR)
-        connstream.close()
+	pass
+        #connstream.shutdown(socket.SHUT_RDWR)
+        #connstream.close()
         
