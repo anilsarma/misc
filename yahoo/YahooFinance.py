f7986036045c1c6f3206f8d8e98ce454ffa96b38
Anil Sarma <anilsarma@yahoo.com>
2020-03-06 16:28:28 -0500

    create a gui in python for the Yahoo data

--- yahoo/YahooFinance.py
+++ yahoo/YahooFinance.py
@@ -1,32 +1,39 @@
-import requests
-import pandas as pd
-import numpy as np
-from bs4 import BeautifulSoup as bs
-import re
-import html
 import datetime
-
+import html
 import io
+import re
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
+
 
 class YahooFinance:
 
     url_base = "https://finance.yahoo.com/quote/{}"
     # start_epoch, end_epoch, event, crumb, interval
     url_hist_base = "https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={start_epoch}&period2={end_epoch}&interval={interval}&events={event}&crumb={crumb}"
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.url_symbol = None
         #elf.symbol = None
         self.cookies = None
         self.crumb = None
         self.summary = {}
+        self.kwargs = kwargs
+        self.query_time = None
+
+    def clear(self):
+        self.crumb = None
+        self.cookies = None
 
     def init(self, symbol):
+        self.kwargs = {}
         #self.start_date = pd.to_datetime(start_date)#.replace(hour=0, minute=0, second=0)
 
         self.url_symbol = YahooFinance.url_base.format(symbol)
         #self.symbol = symbol
         #print(self.url_symbol," startdate=", YahooFinance.get_epoch(self.start_date), self.symbol)
-        with  requests.get(self.url_symbol) as response:
+        with  requests.get(self.url_symbol, *self.kwargs) as response:
             print(response.cookies)
             for x in response.cookies:
                 print("Entries", x.name, x.value)
@@ -64,7 +71,7 @@ class YahooFinance:
                                                 event=event, crumb=self.crumb, interval=interval)
 
         cookies = { x.name: x.value for x in self.cookies }
-        response = requests.get(url, cookies=cookies)
+        response = requests.get(url, cookies=cookies, *self.kwargs)
         df = pd.read_csv(io.StringIO(response.text))
         df.loc[:, 'Symbol']=symbol
         return df
@@ -78,12 +85,18 @@ class YahooFinance:
     def get_historical_split(self, symbol, start_date, end_date=None):
         return self.get_hist_data(symbol, start_date, end_date, event="split    ", interval="1d")
 
-    def get_details(self, symbol):
-        if self.crumb is None or self.cookies.is_expired():
+    def get_details(self, symbol, timedelta=None):
+        if self.crumb is None : #or self.cookies.is_expired():
             self.init(symbol)
+
+        if timedelta is not None and self.query_time is not None:
+            diff = pd.to_datetime(datetime.datetime.now()) - self.query_time
+            if diff < timedelta:
+                return
+
         url_symbol = YahooFinance.url_base.format(symbol)
         cookies = {x.name: x.value for x in self.cookies}
-        response = requests.get(url_symbol, cookies=cookies)
+        response = requests.get(url_symbol, cookies=cookies, *self.kwargs)
         parser = bs(response.text, 'html.parser')
 
         tags = ["PREV_CLOSE-value", "OPEN-value", "BID-value", "ASK-value", "TD_VOLUME-value",
@@ -91,29 +104,113 @@ class YahooFinance:
                 "EARNINGS_DATE-value",
                 "ONE_YEAR_TARGET_PRICE-value", "EX_DIVIDEND_DATE-value"]
         for t in tags:
-            td = parser.find("td", { "data-test": t})
-            value = td.find("span").string
-            print (t, "=", value)
-            self.summary[t] = value
+            try:
+                td = parser.find("td", { "data-test": t})
+                value = td.find("span").string
+                print (t, "=", value)
+                self.summary[t] = value
+            except:
+                self.summary[t] = np.nan
 
         tags = ["DAYS_RANGE-value", "FIFTY_TWO_WK_RANGE-value",  "DIVIDEND_AND_YIELD-value",
                 ]
         for t in tags:
-            td = parser.find("td", { "data-test": t})
-            value = td.string
-            print (t, "=", value)
-            self.summary[t] = value
-
+            try:
+                td = parser.find("td", { "data-test": t})
+                value = td.string
+                print (t, "=", value)
+                self.summary[t] = value
+            except:
+                self.summary[t] = np.nan
         # get the current quotes.
         div = parser.find("div", {"id": "quote-header-info"})
         spans = div.findAll("span")
         current = spans[1].string
-        self.summary['LAST_PRICE']=float(current)
+        self.summary['LAST_PRICE']=self.to_float(current)
+        self.summary['CLOSED'] = False
+        try:
+            div = parser.find("div", {"id": "quote-header-info"})
+            spans = div.findAll("span")
+            time_field = spans[3].string
+            if "At Close".upper() in str(time_field).upper():
+                self.summary["CLOSED"] = True
+        except:
+            pass
 
+        self.query_time = pd.to_datetime(datetime.datetime.now())
+        return self.summary
+
+    def get_query_time(self):
+        return self.query_time
+
+    def get_tuple_value(self, symbol, key, token=None):
+        if not self.summary:
+            self.get_details(symbol)
+        if key in self.summary:
+            if token is None:
+                return self.summary.get(key)
+            bid, qty = self.summary.get(key).split(token)
+            bid = self.to_float(bid)
+            qty = self.to_int(qty)
+            return (bid, qty)
+        if token is None:
+            return "0"
+        return (0,0)
+
+    def to_float(self, v):
+        try:
+            if isinstance(v, str):
+                v = v.replace(",", "")
+            return float(v)
+        except:
+            pass
+        return np.nan
+
+    def to_int(self, v):
+        try:
+            if isinstance(v, str):
+                v = v.replace(",", "")
+            if isinstance(v, float):
+                return v
+            return int(v)
+        except:
+            pass
+        return 0
+
+    def get_last_price(self, symbol):
+        return self.to_float(self.get_tuple_value(symbol, "LAST_PRICE"))
+
+    def get_bid(self, symbol):
+        return self.get_tuple_value(symbol, "BID-value", "x")
+
+    def get_ask(self, symbol):
+        return self.get_tuple_value(symbol, "ASK-value", "x")
+
+    def get_open_price(self, symbol):
+        return self.to_float(self.get_tuple_value(symbol, "OPEN-value"))
+
+    def get_prev_close(self, symbol):
+        return self.to_float(self.get_tuple_value(symbol, "PREV_CLOSE-value"))
+    def get_thirty_day_volume(self, symbol):
+        return self.to_float(self.get_tuple_value(symbol, "TD_VOLUME-value").replace(",", ""))
+
+    def get_close_status(self, symbol):
+        if self.crumb is None:
+            self.init(symbol)
+        if not self.summary:
+            self.get_details(symbol)
+        return self.summary['CLOSED']
 
 if __name__ == "__main__":
     y = YahooFinance()
     #df = y.get_historical("2012.02.28", "2020.02.28")
     #print(df)
     y.get_details("AAPL")
-    df = y.get_historical("AAPL","1970.02.28")
+    df = y.get_historical("AAPL","1970.01.01")
+    df.loc[:, "30D"] = df.Volume.rolling(window=30).mean().fillna(0).astype(int)
+
+    print(df.head(60))
+
+    print(y.get_bid("AAPL"), y.get_ask("AAPL"))
+    print(y.get_open_price("AAPL"))
+    print(y.get_thirty_day_volume("AAPL"))
---
+++ yahoo/YahooGui.py
@@ -0,0 +1,293 @@
+import datetime
+import pandas as pd
+import PyQt5
+
+import  yahoo.YahooFinance as yp
+import sys
+import traceback
+import time
+import os
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+timeout = pd.Timedelta("5 seconds")
+open_time = "9:30"
+close_time = "16:00"
+
+class WorkerSignals(QObject):
+    finished = PyQt5.QtCore.pyqtSignal(object)
+    error = PyQt5.QtCore.pyqtSignal(tuple)
+    result = PyQt5.QtCore.pyqtSignal(object, object)
+
+class Worker(QRunnable):
+    def __init__(self, symbol, parent, proxies):
+        super(Worker, self).__init__()
+        self.symbol = symbol
+        self.parent = parent
+        self.proxies = proxies
+
+        self.signals = WorkerSignals()
+
+    @pyqtSlot()
+    def run(self):
+        try:
+            if self.symbol not in self.parent.yp_cache:
+                print("Checking cache ... ")
+                self.parent.yp_cache[self.symbol] = yp.YahooFinance(proxies=self.proxies, verify=False)
+            yahoo = self.parent.yp_cache [self.symbol]
+            yahoo.get_details(self.symbol, timedelta=timeout)
+
+            bid = yahoo.get_bid( self.symbol)
+            ask = yahoo.get_ask( self.symbol)
+            last = yahoo.get_last_price(self.symbol)
+            qtime = yahoo.get_query_time()
+
+
+            open = yahoo.get_open_price(self.symbol)
+            prev_close = yahoo.get_prev_close(self.symbol)
+            close_status  = yahoo.get_close_status(self.symbol)
+            change = (last - prev_close)/prev_close
+            change = str(int(change * 10000)/100) + "%"
+            result = { 'bid': bid, 'ask': ask, 'last': last, 'open': open, 'close': prev_close, 'change': change, 'close_status': close_status, 'time': qtime}
+            self.signals.result.emit (self.symbol, result)
+        except:
+            traceback.print_exc()
+            exectype, value = sys.exc_info()[:2]
+            self.signals.error.emit( (exectype, value, traceback.format_exc()))
+        finally:
+            self.signals.finished.emit (self.symbol)
+
+
+
+
+class App(QWidget):
+    def __init__(self, proxies=None):
+        super(App, self).__init__()
+        self.proxies = proxies
+        self.title = "Yahoo Maket Prices " + time.ctime(os.path.getmtime(__file__))
+        self.left = 100
+        self.top = 200
+        self.width = 400
+        self.height = 150
+        self.yp_cache = {}
+        self.threadpool = QThreadPool()
+        self.pending_req = {}
+        self.header_map = {}
+
+
+        self.initUI()
+        self.setMinimumSize(1000, 300)
+        #// setWindowIcon(QIcon("png"))
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.recurring_timer)
+        self.timer.start()
+
+    def recurring_timer(self):
+        global timeout
+        try:
+            ot = pd.to_datetime(open_time)
+            ct = pd.to_datetime(close_time)
+            now = pd.to_datetime(datetime.datetime.now())
+            if now >= ot and now <= ct:
+                timeout = pd.Timedelta("5 seconds")
+            else:
+                timeout = pd.Timedelta("15 minutes")
+            self.check_for_expired()
+        except:
+            traceback.print_exc()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.layout = QVBoxLayout(self)
+
+        self.setLayout(self.layout)
+        self.symbol_edit = QLineEdit("", self)
+        hlayout= QHBoxLayout(self)
+        hlayout.stretch(True)
+        hlayout.addWidget(self.symbol_edit)
+        self.add_symbol_button = QPushButton("Add Symbol", self)
+        self.add_symbol_button.clicked.connect(self.on_add_symbol)
+        hlayout.addWidget(self.add_symbol_button)
+        self.layout.addLayout(hlayout)
+
+        model = QStandardItemModel()
+        self.headers = ["Source", "Symbol", "Prev Close", "OPEN", "BidQty", "Bid", "SynP", "Ask", "AskQty", "change", "Status", "timestamp", "age"]
+        model.setHorizontalHeaderLabels(self.headers)
+        table = QTableView()
+        table.setModel(model)
+        table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        table.setSelectionMode(QAbstractItemView.SingleSelection)
+        model.setColumnCount(len(self.headers))
+        self.layout.addWidget(table)
+        self.model= model
+
+        model.appendRow(self.get_details("OEF"))
+       # table.verticalHeader().setDefaultSelectionSize(20)
+        table.verticalHeader().setVisible(False)
+        [table.horizontalHeader().setSectionResizeMode(i, QHeaderView.Stretch) for i in range(0, len(self.headers))]
+
+        self.refresh_button = QPushButton("Refresh", self)
+        self.refresh_button.setToolTip("Refresh Market data")
+        self.layout.addWidget(self.refresh_button)
+        self.refresh_button.clicked.connect(self.on_click_refresh)
+
+
+        self.show()
+
+
+    def check_for_expired(self):
+        #print("check for expired ", self.model.rowCount())
+        for x in range(0, self.model.rowCount()):
+            index = self.model.index(x, self.get_row_index("Symbol"))
+            symbol = index.data()
+            #print("checking for ", symbol)
+            timestamp = pd.to_datetime(self.model.index(x, self.get_row_index("timestamp")).data())
+            now = pd.to_datetime(datetime.datetime.now())
+            if timestamp is not None:
+                delta  = (now - timestamp)
+                if delta < timeout:
+                    #print("too young", self.delta_to_minutes(delta), symbol)
+                    index = self.model.index(x, self.get_row_index("age"))
+                    self.model.setData(index, self.delta_to_minutes(delta))
+                    continue
+            if symbol in self.pending_req:
+                continue
+
+            index = self.model.index(x, self.get_row_index("Source"))
+            self.model.setData(index, QBrush(Qt.gray), Qt.BackgroundRole)
+            self.pending_req[symbol] = symbol
+
+            try:
+                worker = Worker(symbol, self, self.proxies)
+                worker.signals.result.connect(self.on_result)
+                worker.signals.finished.connect(self.on_complete)
+
+                self.threadpool.start(worker)
+            except:
+                traceback.print_exc()
+                del self.pending_req[symbol]
+
+
+
+
+    @pyqtSlot()
+    def on_click_refresh(self):
+        pass
+
+    @pyqtSlot()
+    def on_add_symbol(self):
+        tokens = self.symbol_edit.text().split(",")
+        try:
+            for value in tokens:
+                value = value.strip()
+                if len(value)==0:
+                    continue
+                found = False
+                for x in range(0, self.model.rowCount()):
+                    index = self.model.index(x, 1)
+                    symbol = index.data()
+                    if symbol ==value:
+                        print(value, "already in the list")
+                        found = True
+                        break
+                if not found:
+                    self.model.appendRow(self.get_details(value))
+        except:
+            traceback.print_exc()
+
+    def delta_to_minutes(self, delta):
+        min = delta.total_seconds()/60
+        min = int(min*100)/100  # round out
+        if min <= 0:
+            return ""
+        return str(min) + " minutes"
+
+
+    def get_row_index(self, fieldname):
+        headers = [self.model.headerData(x, Qt.Horizontal) for x in range(0, self.model.columnCount())]
+        if fieldname not in self.header_map:
+            i = 0
+            for x in headers:
+                self.header_map[x] = i
+                i = i +1
+            if fieldname not in self.header_map:
+                return -1
+        return self.header_map[fieldname]
+
+    def set_color(self, index, color):
+        self.model.setData(index, QBrush(color), Qt.BackgroundRole)
+    def on_result(self, symbol, result):
+        try:
+            print("on_result", symbol)
+            headers = [self.model.headerData(x, Qt.Horizontal) for x in range(0, self.model.columnCount())]
+            now = pd.to_datetime(datetime.datetime.now())
+            mapping = {
+                "Status": lambda x:"OPEN" if not x['close_status'] else "CLOSED",
+                "Source": lambda x: "YAHOO-THD",
+                'Prev Close': lambda  x:x['close'],
+                "Symbol": lambda  x: symbol,
+                "age": lambda  x: self.delta_to_minutes(now - pd.to_datetime(x['time'])),
+                'OPEN': lambda x: x['open'],
+                'BidQty': lambda x: x['bid'][1],
+                'Bid': lambda x: x['bid'][0],
+                'SynP': lambda x: x['last'],
+                'AskQty': lambda x: x['ask'][1],
+                'Ask': lambda x: x['ask'][0],
+                'change': lambda x: x['change'],
+                'timestamp': lambda x: x['time'],
+            }
+            data =  [ mapping[x](result) if x in mapping else "" for x in headers]
+            details = [ QStandardItem(str(x)) for x in data]
+            [d.setTextAlignment(Qt.AlignCenter) for d in details]
+            src_index = self.get_row_index("Source")
+            status_index = self.get_row_index("Status")
+            change_index = self.get_row_index("change")
+            for x in range(0, self.model.rowCount()):
+                index = self.model.index(x, 1)
+                table_symbol = index.data()
+                if table_symbol != symbol:
+                    continue
+                for y in range(0, len(details)):
+                    index = self.model.index(x, y)
+                    self.model.setData(index, details[y].text())
+                    if y == src_index:
+                        tm = mapping['timestamp'](result)
+                        delta = now - pd.to_datetime(tm)
+                        self.set_color(index, Qt.green if delta < (timeout + pd.Timedelta("10 seconds")) else Qt.gray)
+                    if y == status_index:
+                        status = self.model.index(x, status_index).data()
+                        self.set_color(index, Qt.green if "OPEN" in status.upper() else Qt.red)
+                    if y == change_index:
+                        status = self.model.index(x, change_index).data()
+                        self.set_color(index, Qt.red if "-" in status.upper() else Qt.green)
+
+        except:
+            traceback.print_exc()
+
+    def on_complete(self, symbol):
+        print("thread complete", symbol)
+        del self.pending_req[symbol]
+
+    def get_details(self, symbol):
+        data = [ QStandardItem("YAHOO"), QStandardItem(symbol)]
+
+        while (len(data) < len(self.headers)):
+            data.append(QStandardItem(""))
+        #data[0].setData(QBrush(Qt.gray), Qt.BackgroundRole)
+        [d.setTextAlignment(Qt.AlignCenter) for d in data]
+        return data
+
+
+
+if __name__ == "__main__":
+    print(open_time, close_time)
+
+    qapp = QApplication(sys.argv)
+    app = App()
+    sys.exit(qapp.exec_())
